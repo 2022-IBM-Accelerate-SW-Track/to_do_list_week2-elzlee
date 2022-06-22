a1b1303d6575aabec0295a6f9d2e85b30cc31cbe
Elizabeth Lee <elizabethlee245@gmail.com>
2022-06-22 16:34:41 -0500

    done

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,32 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import elizabeth_lee from "../assets/elizabeth_lee.JPG";
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
         <div class="split left">
           <div className="centered">
-            <img
+            <img 
               className="profile_image"
-              src={profile_pic}
+              src = {elizabeth_lee}
               alt="Profile Pic"
-            ></img>
+              ></img>
           </div>
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
+            <div className="name_title">Elizabeth Lee</div>
             <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+              Hi, I'm Elizabeth! I'm a rising junior at Carnegie Mellon University
+              studying Information Systems, Human-Computer Interaction, and Computer Science.
+              Outside classes, I like to play the piano and drive for my CMU Buggy team.
+              This summer, I'm a Product Management intern for HP Inc. in Houston, TX!
             </div>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,8 @@ class Home extends Component {
       todos: [],
     };
   }
+
+
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
   // updates the state with the new list.
   addTodo = (todo) => {
@@ -19,14 +21,35 @@ class Home extends Component {
     // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
-    todo.id = Math.random();
-    // Create a array that contains the current array and the new todo item
-    let new_list = [...this.state.todos, todo];
-    // Update the local state with the new array.
+  
+    //check if a task already exists before adding it to Todo list
+    if (this.state.todos.find(t => t.content === todo.content)) {
+      return;
+    } else {
+      todo.id = Math.random();
+      // Create a array that contains the current array and the new todo item
+      let new_list = [...this.state.todos, todo];
+    
+      // Update the local state with the new array.
+      this.setState({
+        todos: new_list,
+      });
+    }
+  };
+
+
+
+  //  remove an item from our Todo list
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
     this.setState({
-      todos: new_list,
+      todos: todos,
     });
   };
+
+
   render() {
     return (
       <div className="Home">
@@ -36,7 +59,9 @@ class Home extends Component {
         <AddTodo addTodo={this.addTodo} />
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} 
+               deleteTodo={this.deleteTodo}/>
+
       </div>
     );
   }
